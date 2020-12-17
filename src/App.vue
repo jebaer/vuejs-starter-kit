5440f301723428ad2ca495c2ba3e53423d51942d
red-hedgehog <jrmybrcf@gmail.com>
2020-12-17 17:11:28 +0100

    custom tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="app" class="background">
+    <div id="app" class="bg-black-base text-white-base">
         <router-view></router-view>
     </div>
 </template>
@@ -9,3 +9,60 @@ export default {
     name: 'App',
 };
 </script>
+
+<style>
+html,
+body,
+#app {
+    height: 100vh;
+}
+
+body,
+#app {
+    min-height: 100vh;
+    margin: 0;
+}
+
+main {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    align-items: center;
+    position: fixed;
+    top: 75px;
+    bottom: 55px;
+    overflow-y: overlay;
+    overflow: scroll;
+    scrollbar-width: none;
+    padding: 50px 0;
+}
+@media screen and (min-width: 767px) {
+    main {
+        top: 115px;
+        bottom: 0px;
+    }
+}
+main::-webkit-scrollbar {
+    display: none;
+}
+
+/* width */
+::-webkit-scrollbar {
+    width: 10px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+    background: #f1f1f1;
+}
+
+/* Handle */
+::-webkit-scrollbar-thumb {
+    background: #888;
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+    background: #555;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,9 @@
 <template>
     <main>
-        <h1>VUEJS STARTER KIT</h1>
-        <h2 class="is-sub-title">
+        <h1 class="text-2xl text-base font-bold">VUEJS STARTER KIT</h1>
+        <h2 class="text-xl text-secondary-base">
             A PRECONFIGURED PACKAGE TO START IN A RIGHT WAY
         </h2>
-        <div class="bg-red-900 text-white ">
-            Test
-        </div>
     </main>
 </template>
 
